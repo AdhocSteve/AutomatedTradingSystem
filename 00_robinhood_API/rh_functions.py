54bf3e663e785de711ca8c92c816a26c0bf09050
AdhocSteve <adhocsteve@outlook.com>
2025-11-26 23:37:29 -0700

    erased important info since this is public

--- 00_robinhood_API/rh_functions.py
+++ 00_robinhood_API/rh_functions.py
@@ -6,10 +6,10 @@ import robin_stocks.robinhood as r
 def rh_login_before():
     totp  = pyotp.TOTP("My2factorAppHere").now()
     print("Current OTP:", totp)
-    login = r.login('adhocsteve@outlook.com','RobinPass99', mfa_code=totp, expiresIn=86400)
+    login = r.login('','', mfa_code=totp, expiresIn=86400)
 
 def rh_login_after():
-    r.login('adhocsteve@outlook.com','RobinPass99',expiresIn=86400)
+    r.login('','',expiresIn=86400)
 
 
 if __name__ == "__main__":
